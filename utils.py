8bf922f27026ba30a502bfbd97309724fd6ec241
jason <thejasonluo@gmail.com>
2013-11-21 00:51:42 -0500

    basic db

---
+++ utils.py
@@ -0,0 +1,17 @@
+from pymongo import MongoClient
+
+
+def auth(user, password, coll):
+    return [ x for x in coll.find ({'username': user, 'password':password})] != []
+
+def addUser(user, password, coll):
+    coll.insert({'username': user, 'password':password}]
+
+def addLocation(user, location):
+    coll.insert({'username': user, 'location': location})
+
+def getLocation(location, coll):
+    return [x for x in coll.find ({'location':location})
+
+def addEvent():
+
