0729e60b531c401602782b1ce3007816cbdc6a71
thejasonluo <thejasonluo@gmail.com>
2013-11-22 12:17:37 -0500

    filled in methods, but unsure if works.

--- utils.py
+++ utils.py
@@ -4,20 +4,23 @@ from pymongo import MongoClient
 def auth(user, password, coll):
     return [ x for x in coll.find ({'username': user, 'password':password})] != []
 
-def addUser(user, password, coll):
-    coll.insert({'username': user, 'password':password}]
+def addUser(user, password, coll): #adds user
+    coll.insert({'username': user, 'password':password})
 
-def addLocation(user, location):
+def addLocation(user, location): #adds location
     coll.insert({'username': user, 'location': location})
 
-def getLocation(location, coll):
+def getLocation(location, coll): #returns location
     return [x for x in coll.find ({'location':location})
 
-def changeLocation(username, location):
+def changeLocation(username, location): #changes location
+    coll.update({'username': user}, {'$set':{'location': 'location'}}) 
     
-def addEvent():
+def addEvent(user, event): #adds events
+    coll.insert({'username':user, 'event': event}) #possibly return values if there is a sucess or not?
 
-def changeName(username, name):
+def changeName(username, name): #changes name
+    coll.update({'username': user}, {'$set':{'name': 'name'}})
     
-def checkUser(username):
-    
+def checkUser(username, coll): #this is auth without the password
+    return [ x for x in coll.find ({'username': user})] != []
