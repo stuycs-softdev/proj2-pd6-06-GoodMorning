89a411932f81a268d7cdaa73685c37019be30c43
rlamchops <lamraymond1996@gmail.com>
2013-11-26 11:40:49 -0500

    Update weather.py

--- weather.py
+++ weather.py
@@ -1,10 +1,13 @@
 import urllib2
 import json
 
+def url():
+    f = urllib2.urlopen('http://api.wunderground.com/api/987077b70105ec11/hourly/q/NY/New_York_City.json')
+    return f
 #-----------------uses weather underground api
 
 def getWeather():
-  f = urllib2.urlopen('http://api.wunderground.com/api/987077b70105ec11/hourly/q/NY/New_York_City.json')
+  f = url()
   json_string = f.read()
   parsed_json = json.loads(json_string)
   weather = parsed_json['hourly_forecast'][0]['condition']
@@ -13,7 +16,7 @@ def getWeather():
     return weather
 
 def getTemp():
-  f = urllib2.urlopen('http://api.wunderground.com/api/987077b70105ec11/hourly/q/NY/New_York_City.json')
+  f = url()
   json_string = f.read()
   parsed_json = json.loads(json_string)
   temp_f = parsed_json['hourly_forecast'][0]['temp']['english']
