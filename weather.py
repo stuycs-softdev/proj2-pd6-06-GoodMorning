cd67a1465af6e7f35c6f7ec85dac91e2c94141ed
rlamchops <lamraymond1996@gmail.com>
2013-11-25 21:24:29 -0500

    screwing around with wunderground api. have not tested yet.

---
+++ weather.py
@@ -0,0 +1,22 @@
+import urllib2
+import json
+
+#-----------------uses weather underground api
+
+def getWeather():
+f = urllib2.urlopen('http://api.wunderground.com/api/987077b70105ec11/hourly/q/NY/New_York_City.json')
+json_string = f.read()
+parsed_json = json.loads(json_string)
+weather = parsed_json['hourly_forecast'][0]['condition']
+f.close()
+#-----------------what the sky should look like (ex: cloudy, rainy, etc.)
+  return weather
+
+def getTemp():
+f = urllib2.urlopen('http://api.wunderground.com/api/987077b70105ec11/hourly/q/NY/New_York_City.json')
+json_string = f.read()
+parsed_json = json.loads(json_string)
+temp_f = parsed_json['hourly_forecast'][0]['temp']['english']
+f.close()
+#-----------------the expected temperature in fahrenheit. celsius is possible by replacing 'english' with 'metric'
+  return temp_f
