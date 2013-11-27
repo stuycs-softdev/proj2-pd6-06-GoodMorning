0e0b07af0bea9a85524eaff0d1ab465cc8d290b1
rlamchops <lamraymond1996@gmail.com>
2013-11-27 12:29:46 -0500

    don

--- mta.py
+++ mta.py
@@ -1,10 +1,22 @@
 import urllib2
 from bs4 import BeautifulSoup
 
+#scraping mta right now, will switch to api if need be
+
 def url():
   f = urllib2.urlopen('http://www.mta.info/status/serviceStatus.txt')
   return f
   
-def test():
+def info():
   f = url()
   result = BeautifulSoup(f)
+  
+  #---------this should return a list of all the train names?
+  lineNames = soup.service.subway.findNextSibling('line').name
+
+  #--------this should return a list of the status of those said trains?  
+  lineStatus = soup.serive.subway.findNextSibling('line').status
+
+  
+  #--------thinking about buses, but that will be for later
+  
