0166a1895ed0f6c9de9a55b232b46b34c2eaa1bf
thejasonluo <thejasonluo@gmail.com>
2013-12-01 13:46:40 -0500

    app.py is not connecting

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, request, render_template, redirect, session
+from flask import Flask
+from flask import request, render_template, redirect, session
 from pymongo import MongoClient
 
 import utils
@@ -12,9 +13,11 @@ app.secret_key = 'jasoniscool'
 @app.route("/")
 def home():
         if "username" in session: #if logged in already
+		username = session ['username']
                 return render_template(homepage.html)
-        else: #if not logged in
-                return redirect("/login")
+         #if not logged in
+	else:
+		return redirect("/login")
 
 
 @app.route("/login",methods=['GET','POST'])
@@ -23,13 +26,14 @@ def login():
                 return render_template("login.html")
         username = request.form['name']
         password = request.form['password']
-if not username or not password:    #there are fields that are empty
-	return render_template(login.html, message = "Please fill out the empty fields!")
-elif utils.auth(username, password, db): #login successful
-	session["username"] = username
-	return redirect("/")
-else: #login unsuccessful
-	return render_template(login.html, message = "Incorrect username and password combination.")
+	if not username or not password:    #there are fields that are empty
+		return render_template(login.html, message = "Please fill out the empty fields!")
+	elif utils.auth(username, password, db): #login successful
+		session["username"] = username
+		return redirect("/")
+ #login unsuccessful
+	else:
+		return render_template(login.html, message = "Incorrect username and password combination.")
 
 @app.route("/register",methods = ["GET","POST"])
 def register():
@@ -73,10 +77,10 @@ def settings():
         
 @app.route("/logout")
 def logout():
-    session.pop("username", None)
-    return redirect("/") #or maybe redirect to login? idk.
+	session.pop("username", None)
+	return redirect("/") #or maybe redirect to login? idk.
 
 
 if __name__=="__main__":
-    app.debug=True
-    app.run(host='0.0.0.0',port=5000)
+	app.debug=True
+	app.run(host='0.0.0.0',port=5000)
--- utils.py
+++ utils.py
@@ -7,20 +7,20 @@ def auth(user, password, coll):
 def addUser(user, password, coll): #adds user
     coll.insert({'username': user, 'password':password})
 
-def addLocation(user, location): #adds location
+def addLocation(user, location, coll): #adds location
     coll.insert({'username': user, 'location': location})
 
 def getLocation(location, coll): #returns location
-    return [x for x in coll.find ({'location':location})
+    return [x for x in coll.find ({'location':location})]
 
-def changeLocation(location): #changes location
-    coll.update({'$set':{'location': 'location'}) 
+def changeLocation(user, location, coll): #changes location
+    coll.update({'username': user}, {'$set':{'location': 'location'}}) 
     
-def addEvent(event): #adds events
-    coll.insert({'event': event}) #possibly return values if there is a sucess or not?
+def addEvent(user, event, coll): #adds events
+    coll.insert({'username': user},{'event': event}) #possibly return values if there is a sucess or not?
 
-def changeName(name): #changes name
-    coll.update({'$set':{'name': 'name'}})
+def changeName(user, name): #changes name
+    coll.update({'username': user},{'$set':{'name': 'name'}})
     
 def checkUser(username, coll): #this is auth without the password
     return [ x for x in coll.find ({'username': user})] != []
