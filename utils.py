71df6dac0c323a755676ac926520a4d69205f326
rlamchops <lamraymond1996@gmail.com>
2013-12-03 18:20:52 -0500

    rewrote (basic, extra) database and commented out old code

--- utils.py
+++ utils.py
@@ -1,34 +1,69 @@
 from pymongo import MongoClient
+from flask import Flask, url_for, request, redirect, render_template
 
-
-def auth(user, password, coll):
-    return [ x for x in coll.find ({'username': user, 'password':password})] != []
-
-def addUser(user, password, coll): #adds user
-    coll.insert({'username': user, 'password':password})
-
-def addLocation(user, location, coll): #adds location
-    coll.insert({'username': user, 'location': location})
-
-def getLocation(location, coll): #returns location
-    return [x for x in coll.find ({'location':location})]
-
-def changeLocation(user, location, coll): #changes location
-    coll.update({'username': user}, {'$set':{'location': 'location'}}) 
+def open():
+    client = MongoClient()
+    db = client["test"]
+    return db
     
-def addEvent(user, year, month, date, hour, minute, title, coll): #adds events
-    coll.insert({'username': user},{'year': year}, {'month': month}, {'date':date}, {'hour': hour}, {'minute':minute},{'title': title}) 
+#------------USER MANAGEMENT---------------------------------------------------
+def register(username, password, nickname):
+    db = open()
+    check = db.test.find_one({'username' : username}, fields={'_id':False})
+    if check == None:
+        db.test.insert({'username' : username, 'password' : password, 'nickname' : nickname})
+        return true
+    else:
+        return false
+        
+def login(username, password):
+    db = open()
+    user = db.test.find_one({'username' : username}, fields={'_id':False})
+    if user is not None and user['password'] == password:
+        return True
+    else:
+        return False
+        
+def checkForName(username):
+    db = open()
+    user = db.test.find_one({'username' : username}, fields={'_id':False})
+    if user is not None:
+        return False
+    else:
+        return True
 
-def getEvent(year, month, date, coll):
-    return coll.find( {'year':year}, {'month':month}, {'date':date})
+#def changePW() ----------might think about implementing this----------
+#def changeNickname() ------------and this-------------later--------
 
-def getMonthList(user, month, year, coll):
-    return coll.find({'user':user}, {'month':month}, {'year':year});
 
-def changeName(user, name): #changes name
-    coll.update({'username': user},{'$set':{'name': 'name'}})
+#def auth(user, password, coll):
+#    return [ x for x in coll.find ({'username': user, 'password':password})] != []
+
+#def addUser(user, password, coll): #adds user
+#    coll.insert({'username': user, 'password':password})
+
+#def addLocation(user, location, coll): #adds location
+#    coll.insert({'username': user, 'location': location})
+
+#def getLocation(location, coll): #returns location
+#    return [x for x in coll.find ({'location':location})]
+
+#def changeLocation(user, location, coll): #changes location
+#    coll.update({'username': user}, {'$set':{'location': 'location'}}) 
     
-def checkUser(username, coll): #this is auth without the password
-    return [ x for x in coll.find ({'username': username})] != []
+#def addEvent(user, year, month, date, hour, minute, title, coll): #adds events
+#    coll.insert({'username': user},{'year': year}, {'month': month}, {'date':date}, {'hour': hour}, {'minute':minute},{'title': title}) 
+
+#def getEvent(year, month, date, coll):
+#    return coll.find( {'year':year}, {'month':month}, {'date':date})
+
+#def getMonthList(user, month, year, coll):
+#    return coll.find({'user':user}, {'month':month}, {'year':year});
+
+#def changeName(user, name): #changes name
+#    coll.update({'username': user},{'$set':{'name': 'name'}})
+    
+#def checkUser(username, coll): #this is auth without the password
+#    return [ x for x in coll.find ({'username': username})] != []
 
 #def getName(username, coll): #this should return the nickname of a user
