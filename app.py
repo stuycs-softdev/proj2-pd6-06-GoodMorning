8be9118780bb67666ff17be148730ce95f7d4747
rlamchops <lamraymond1996@gmail.com>
2013-12-04 19:18:29 -0500

    looking into fixing up settings()

--- app.py
+++ app.py
@@ -262,6 +262,12 @@ def settings():
         train3 = request.form.get("train3")
         email = request.form['email']
         button =  request.form.get("submit")
+	print nickname
+	print train1
+	print train2
+	print train3
+	print email
+	print button
         #location = request.form['location'] #where you are
         #button = request.form['submit'] #this will be whichever button you've pressed: to change name or location
         #oldPW = request.form['oldPW']
@@ -271,7 +277,7 @@ def settings():
           #      return redirect("/login")
         if button == "submit":
                 utils.updateSettings(session["username"], nickname, train1, train2, train3, email)
-                return redirect("/homepage")
+                return render_template("settings.html", message = "Done.")
                 #to change name
         #if button == 'changeLocation':
         #        utils.changeLocation(session["username"], name) # to change location
