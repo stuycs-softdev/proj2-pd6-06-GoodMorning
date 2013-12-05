60563cf65b625ac39829584bbff845f7a0cc17c3
vgreene98 <topaz.q98@gmail.com>
2013-12-05 06:51:39 +0000

    zeroing radicality

--- app.py
+++ app.py
@@ -79,7 +79,7 @@ def cal():
             de = request.form['starthour'] + ":" + request.form['startmin'] + request.form['amorpm1'] + "-" + request.form['endhour'] + ":" + request.form['endmin'] + request.form['amorpm2'] + " - " + request.form['newevent']
         else:
             de = request.form['newevent']
-	e = Event(y,m+1,d,h,mi,de)
+	e = Event(y,m,d,h,mi,de)
 
         utils.addEventObject(session["username"], e)
 
@@ -118,7 +118,7 @@ def getCal(year, month):
             de = request.form['starthour'] + ":" + request.form['startmin'] + request.form['amorpm1'] + " - " + request.form['newevent']
         else:
             de = request.form['newevent']
-        e = Event(year,month+1,d,h,mi,de)
+        e = Event(year,month,d,h,mi,de)
 
         utils.addEventObject(session["username"],e)
 
