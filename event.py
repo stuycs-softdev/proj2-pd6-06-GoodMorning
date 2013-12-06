64622a2b9d0533419d77d309f66efbcf0293f6df
Victoria Greene <topaz.q98@gmail.com>
2013-12-06 01:16:12 -0500

    updated display of dates in event.py

--- event.py
+++ event.py
@@ -4,9 +4,41 @@ class Event():
     def __init__(self, y, m, d, hour, minute, title):
         self.date = datetime(y, m, d, hour=hour, minute=minute)
         self.title = title
+        self.datestr = str(self.date)
+        mstr = ""
+        if m == 1:
+            mstr = "Jan"
+        elif m == 2:
+            mstr = "Feb"
+        elif m == 3:
+            mstr = "Mar"
+        elif m == 4:
+            mstr = "Apr"
+        elif m == 5:
+            mstr = "May"
+        elif m == 6:
+            mstr = "Jun"
+        elif m == 7:
+            mstr = "Jul"
+        elif m == 8:
+            mstr = "Aug"
+        elif m == 9:
+            mstr = "Sep"
+        elif m == 10:
+            mstr = "Oct"
+        elif m == 11:
+            mstr = "Nov"
+        elif m == 12:
+            mstr = "Dec"
+        today = str(datetime.today())
+        evdate = str(self.date)
+        if today[0:10] == evdate[0:10]:
+            self.datestr = "Today" 
+        else:
+            self.datestr = mstr + " " + str(d)
         
     def __str__(self):
-        return '%s at %s'%(self.title, self.date)
+        return "%s - %s, real date: %s"%(e.datestr, e.title, e.date)
 
     def should_show(self):
         today = datetime.today()
@@ -21,10 +53,15 @@ class Event():
 #events = [Event(1000, 1, 27, 5, 00, "hello")]
 
 def form(e):
-    return "%s at %s"%(e.title, e.date)
+    return "%s - %s, real date: %s"%(e.datestr, e.title, e.date)
 
 def organize(events):
     today = datetime.today()
     events = sorted([e for e in events if e.should_show()], key=lambda e: e.date)
     return events
 
+e = Event(2013, 1, 23, 1, 2, "Today do this")
+print form(e)
+
+e1 = Event(2013, 12, 6, 1, 2, "lalalalal")
+print form(e1)
